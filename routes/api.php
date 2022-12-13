47458c62b45aa97fe10e013756ed20bc48b85626
danadoraiva1985 <mtedic@yahoo.com>
2022-12-13 21:51:01 +0100

    Commit

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\GalleriesController;
+use App\Http\Controllers\CommentsController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -27,3 +29,11 @@ Route::controller(AuthController::class)->group(
         Route::post('logout', 'logout');
     }
 );
+
+Route::resource('galleries', GalleriesController::class);
+Route::get('/galleries/{id}/comments', [CommentsController::class, 'index']);
+Route::get('/comments/{id}', [CommentsController::class, 'show']);
+Route::post('/galleries/{id}/comments', [CommentsController::class, 'store']);
+Route::delete('/comments/{id}', [CommentsController::class, 'destroy']);
+Route::get('/user/{id}', [UserController::class, 'show']);
+
